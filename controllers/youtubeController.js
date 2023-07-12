22a7ccc37fdd479cd8601a6005119d470754f359
Juan Cecenas <juancarlos.cecenarojas@gmail.com>
2023-07-12 14:58:37 -0600

    Format result to display as requested

--- controllers/youtubeController.js
+++ controllers/youtubeController.js
@@ -1,11 +1,27 @@
 const youtubeService = require('../services/youtubeService');
 
+const formatResult = (result) => {
+  const { id, snippet } = result;
+  
+  return {
+    published_at: snippet.publishedAt,
+    id: id.videoId,
+    title: snippet.title,
+    description: snippet.description,
+    thumbnail: snippet.thumbnails.default.url,
+    extra: {
+      something: "extra"
+    }
+  };
+};
+
 const searchYouTube = async (req, res) => {
   const searchQuery = req.query.search;
 
   try {
     const results = await youtubeService.searchVideos(searchQuery);
-    res.status(200).json(results);
+    const formattedResults = results.map(formatResult);
+    res.status(200).json(formattedResults);
   } catch (error) {
     console.error('Search failed: ', error);
     res.status(500).json({ error: 'Search failed' });
@@ -14,4 +30,4 @@ const searchYouTube = async (req, res) => {
 
 module.exports = {
   searchYouTube,
-}
+};
